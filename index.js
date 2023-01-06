baaf7a60c65612918b1a34690c9c5c444323bab3
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-01-06 23:32:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const commands = require('./commands.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.commands = new Collection();
+
+for (const command of commands) {
+	client.commands.set(command.data.name, command);
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands.map(command => command.data.toJSON()) },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.login(token);
