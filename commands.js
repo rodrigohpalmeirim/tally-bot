1659bb0715faef73f7c21e1939efeaa476ef8c31
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-01-08 02:13:46 +0000

    refactor & add expense command

--- commands.js
+++ commands.js
@@ -1,12 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = [
-	{
-		data: new SlashCommandBuilder()
-			.setName('ping')
-			.setDescription('Replies with Pong!'),
-		async execute(interaction) {
-			await interaction.reply('Pong!');
-		},
-	},
+	new SlashCommandBuilder()
+		.setName('expense')
+		.setDescription('Add a shared expense')
+		.addStringOption(option =>
+			option.setName('title')
+				.setDescription('The title of the expense')
+				.setRequired(true),
+		)
+		.addNumberOption(option =>
+			option.setName('amount')
+				.setDescription('The amount of the expense')
+				.setRequired(true),
+		),
 ];
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
+const { Client, Events, GatewayIntentBits, REST, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle, UserSelectMenuBuilder } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const commands = require('./commands.js');
 
@@ -8,44 +8,85 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.commands = new Collection();
-
-for (const command of commands) {
-	client.commands.set(command.data.name, command);
-}
+const newExpenses = {}
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
 	try {
-		await command.execute(interaction);
+		if (interaction.isChatInputCommand()) { // Handle slash commands
+			if (interaction.commandName === 'expense') {
+				newExpenses[interaction.id] = {
+					title: interaction.options.getString('title'),
+					amount: interaction.options.getNumber('amount')
+				};
+				await interaction.reply({
+					ephemeral: true,
+					components: [
+						new ActionRowBuilder().addComponents(new UserSelectMenuBuilder().setCustomId(`main-user_${interaction.id}`).setPlaceholder('Who paid the expense?')),
+						new ActionRowBuilder().addComponents(new UserSelectMenuBuilder().setCustomId(`involved-users_${interaction.id}`).setPlaceholder('Who is the expense for?').setMaxValues(25)),
+						new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId(`add-expense_${interaction.id}`).setLabel('Add expense').setStyle(ButtonStyle.Primary).setDisabled(true)),
+					]
+				});
+			}
+		} else if (interaction.isUserSelectMenu()) { // Handle select menus
+			const { customId } = interaction;
+			const [action, id] = customId.split('_');
+			if (action === 'main-user') {
+				newExpenses[id].mainUser = interaction.values[0];
+			} else if (action === 'involved-users') {
+				newExpenses[id].involvedUsers = interaction.values;
+			}
+			interaction.update({
+				components: [
+					new ActionRowBuilder().addComponents(new UserSelectMenuBuilder().setCustomId(`main-user_${id}`).setPlaceholder('Who paid the expense?').setMinValues(0)),
+					new ActionRowBuilder().addComponents(new UserSelectMenuBuilder().setCustomId(`involved-users_${id}`).setPlaceholder('Who is the expense for?').setMinValues(0).setMaxValues(25)),
+					new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId(`add-expense_${id}`).setLabel('Add expense').setStyle(ButtonStyle.Primary).setDisabled(!newExpenses[id].mainUser || !newExpenses[id].involvedUsers?.length)),
+				]
+			});
+		} else if (interaction.isButton()) { // Handle buttons
+			const { customId } = interaction;
+			const [action, id] = customId.split('_');
+			if (action === 'add-expense') {
+				const { mainUser, involvedUsers, title, amount } = newExpenses[id];
+				await interaction.update({ content: 'Expense added!', components: [] });
+				await interaction.channel.send({
+					content: `${interaction.user} added an expense:`,
+					"embeds": [
+						{
+							"type": "rich",
+							"title": title,
+							"fields": [
+								{
+									"name": `Amount:`,
+									"value": `€${amount}`,
+									"inline": true
+								},
+								{
+									"name": `Paid by:`,
+									"value": `<@${mainUser}>`,
+									"inline": true
+								},
+								{
+									"name": `For${involvedUsers.length > 1 ? ` ${involvedUsers.length} people` : ""}:`,
+									"value": `${involvedUsers.map(user => `<@${user}>`).join(' ')}`,
+									"inline": involvedUsers.length == 1
+								}
+							]
+						}
+					]
+				});
+			}
+		}
 	} catch (error) {
 		console.error(error);
 	}
 });
 
 const rest = new REST({ version: '10' }).setToken(token);
-
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands.map(command => command.data.toJSON()) },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		console.error(error);
-	}
-})();
+rest.put(
+	Routes.applicationGuildCommands(clientId, guildId),
+	{ body: commands.map(command => command.toJSON()) },
+)
+.then(data => console.log(`Successfully reloaded ${data.length} application (/) commands.`))
+.catch(console.error);
 
 client.login(token);
